9a3bb44781c4f20ade7cd74193174924d508dc41
MSCunha <matheusscunha9@gmail.com>
2024-07-12 16:35:57 -0300

    .

--- public/script.js
+++ public/script.js
@@ -1,50 +1,49 @@
+import firebase from "firebase/compat/app";
 
 /*const recoverpsw=document.getElementById('recoverpsw');
 const login=document.getElementById('login');
 const registrar=document.getElementById('registrar');
 const loginform=document.getElementById('loginform');
-const regfom=document.getElementById('regform');*/
-
-import firebase from "firebase/compat/app";
-
-document.getElementById('registrar').addEventListener('click', function() {
-	form.loginform().style.display = 'none';
-	form.regform().style.display = 'flex';
-});
-
-document.getElementById('retornar').addEventListener('click', function() {
-	form.loginform().style.display = 'flex';
-	form.regform().style.display = 'none';
-});
+const regform=document.getElementById('regform');*/
 
 document.addEventListener('DOMContentLoaded', function() {
-	form.menuToggle().addEventListener('click', function() {
-			const sidebar = document.getElementById('sidebar');
-			if (sidebar.style.left === '-200px') {
-					sidebar.style.left = '0';
-					document.querySelector('.content').style.marginLeft = '200px';
-			} else {
-					sidebar.style.left = '-200px';
-					document.querySelector('.content').style.marginLeft = '0';
-			}
+	document.getElementById('registrarbtn').addEventListener('click', function() {
+		form.loginform().style.display = 'none';
+		form.regform().style.display = 'flex';
 	});
+	
+	document.getElementById('retornar').addEventListener('click', function() {
+		form.loginform().style.display = 'flex';
+		form.regform().style.display = 'none';
+	});
+	
+    form.menuToggle().addEventListener('click', function() {
+        const sidebar = document.getElementById('sidebar');
+        if (sidebar.style.left === '-200px') {
+            sidebar.style.left = '0';
+            document.querySelector('.content').style.marginLeft = '200px';
+        } else {
+            sidebar.style.left = '-200px';
+            document.querySelector('.content').style.marginLeft = '0';
+        }
+    });
 });
 
-recoverpsw.addEventListener('click', function(){
-	showLoading();
-	firebase.auth().sendPasswordResetEmail(form.email().value).then(()=>{
-		hideLoading();
-		alert("Email enviado com sucesso!");
-	}).catch(error =>{
-		hideLoading();
-	})
+document.getElementById('recoverpsw').addEventListener('click', function(){
+    showLoading();
+    firebase.auth().sendPasswordResetEmail(form.email().value).then(()=>{
+        hideLoading();
+        alert("Email enviado com sucesso!");
+    }).catch(error =>{
+        hideLoading();
+    })
 });
-														
+
 function redirectHome(){
-	loginform.classList.add('fade-out');
-	setTimeout(function(){
-		window.location.href = 'home.html';
-	}, 1000);
+    form.loginform().classList.add('fade-out');
+    setTimeout(function(){
+        window.location.href = 'home.html';
+    }, 1000);
 }
 
 /*
@@ -71,21 +70,20 @@ function validateEmail(email) {
 */
 
 const form = {
-	email:() =>document.getElementById('email'),
-	password:() =>document.getElementById('password'),
-	login:() =>document.getElementById('login'),
-	recoverpsw:() =>document.getElementById('recoverpsw'),
-	registrar:() =>document.getElementById('registrar'),
-	loginform:() =>document.getElementById('loginform'),
-	regfom:() =>document.getElementById('regform'),
-	menuToggle:() =>document.getElementById('menuToggle'),
-}
+    email: () => document.getElementById('email'),
+    password: () => document.getElementById('password'),
+    loginbtn: () => document.getElementById('loginbtn'),
+    recoverpsw: () => document.getElementById('recoverpsw'),
+    registrarbtn: () => document.getElementById('registrarbtn'),
+    loginform: () => document.getElementById('loginform'),
+    regform: () => document.getElementById('regform'),
+    menuToggle: () => document.getElementById('menuToggle'),
+};
 
 function showLoading(){
-
-
+    document.querySelector('.loading').hidden = false;
 }
 
 function hideLoading(){
-
+    document.querySelector('.loading').hidden = true;
 }
